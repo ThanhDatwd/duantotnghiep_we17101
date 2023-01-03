22e28c2ea939ef393d51a1eb96120b91bf1c5a8c
Thi <vuphungthi1@gmail.com>
2023-01-03 17:46:50 +0700

    thêm file seeder đồ ăn

---
+++ database/seeders/products20.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class products20 extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $thit='Thịt';
+        $tenthit=['gà Đông Tảo', 'cá hồi', 'nai','cá'];
+       
+
+        for($i=0;$i<5;$i++){
+            $name=$thit.' '.$tenthit[rand(0,3)];
+           
+         
+            
+            $thumb=rand(1,5).'.jpg';
+            DB::table('products')->insert([
+                "name"=>$name,
+                "thumb"=>"https://loremflickr.com/320/240/food?random=".$thumb,
+                "summary"=>"Đây là đoạn giới thiệu tóm tắt sản phẩm",
+                "content"=>"Nội dung chính giới  thiệu về sản phẩm ",
+                "price"=>100000,
+                "price_format"=>"100.000 đ",
+                "price_current"=>100000,
+                "price_current_format"=>"100.000 đ",
+                "quantity_input"=>100,
+                "quantity_output"=>10,
+                "discount"=>5,
+                "brand"=>"Đà Lạt",
+                "unit"=>"kg",
+                "color"=>'',
+                "slug"=>'food-'.rand(1,1000)
+            ]);
+        }
+
+
+
+
+
+
+    }
+}
