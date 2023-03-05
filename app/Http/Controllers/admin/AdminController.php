5eda6af25062c42564b705794de14e4dcfa45c46
Thi <vuphungthi1@gmail.com>
2023-03-05 22:52:39 +0700

    giao diện thống kê

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\product;
+use App\Models\news;
+// đơn hàng
+use App\Models\order;
+// khách hàng
+use App\Models\customer;
 
 class AdminController extends Controller
 {
     //
-    public function index(){
-        return view('admin.home.index');
+public function index(){
+    $news = News::all();
+    $products = Product::all();
+    $orders = Order::all();
+    $totalProducts = count($products);
+    $totalNews = count($news);
+    $totalOrders = count($orders);;
+
+    return view('admin.home.index', compact('news', 'products', 'totalProducts', 'totalNews', 'orders', 'totalOrders'));
+}
+    // thống kê order
+    public function order(){
+      //
     }
 }
