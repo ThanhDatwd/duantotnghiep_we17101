3d21b8a52e10a1b5a40da3348cacd3acd0f0fef6
nguyenthanhdat <87852737+ThanhDatwd@users.noreply.github.com>
2023-03-22 20:26:01 +0700

    d

--- app/Http/Controllers/client/ContactController.php
+++ app/Http/Controllers/client/ContactController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\client;
 
 use App\Http\Controllers\Controller;
+use App\Mail\ContactMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
     public function contact()
     {
         return view('client.contact.index');
+    }
+    public function contact_(Request $request)
+    {
+        // $request->us
+        // $mail=new co
+        $mail=new ContactMail();
+        Mail:: to($request->email)->queue($mail);
     }
 }
